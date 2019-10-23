08d7fd9295915dfc5d27a89fe12e0a884d16c8f4
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-10-23 13:13:05 +0400

    Logout

--- src/main/java/services/ravi/user/crm/config/WebSecurityConfig.java
+++ src/main/java/services/ravi/user/crm/config/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+            .antMatchers("/login/success").authenticated()
             .antMatchers("/**").permitAll()
             .anyRequest().authenticated()
             .and() // Adding new Rule.
